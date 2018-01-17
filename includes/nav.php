977f84c9139cda2421d1a0f546026398e8ce7c80
Will Jones <signups1993@gmail.com>
2018-01-17 11:42:20 +0000

    Added breadcrumbs

--- includes/nav.php
+++ includes/nav.php
@@ -3,8 +3,47 @@
     <a href="#" class="brand-logo">Logo</a>
     <ul id="nav-mobile" class="right hide-on-med-and-down">
       <li><a href="users/login.php">Log in</a></li>
-      <li><a href="badges.html">Components</a></li>
+      <li><a href="something.php">Test Breadcrumbs</a></li>
       <li><a href="collapsible.html">JavaScript</a></li>
     </ul>
+  </div>
+</nav>
+
+<nav>
+  <div class="nav-wrapper">
+    <div class="col s12">
+      <?php
+      function breadcrumbs($separator = ' &raquo; ', $home = 'Home') {
+
+          $path = array_filter(explode('/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)));
+          $base_url = substr($_SERVER['SERVER_PROTOCOL'], 0, strpos($_SERVER['SERVER_PROTOCOL'], '/')) . '://' . $_SERVER['HTTP_HOST'] . '/';
+          $breadcrumbs = array("<a href=\"$base_url\">$home</a>");
+          $tmp = array_keys($path);
+          $last = end($tmp);
+          unset($tmp);
+
+          foreach ($path as $x => $crumb) {
+              $title = ucwords(str_replace(array('.php', '_'), array('', ' '), $crumb));
+      	if ($x == 1){
+      	        $breadcrumbs[]  = "<a href=\"$base_url$crumb\">$title</a>";
+      	}elseif ($x > 1 && $x < $last){
+      		$tmp = "<a href=\"$base_url";
+      		for($i = 1; $i <= $x; $i++){
+      			$tmp .= $path[$i] . '/';
+      		}
+                      $tmp .= "\">$title</a>";
+      		$breadcrumbs[] = $tmp;
+      		unset($tmp);
+              }else{
+                      $breadcrumbs[] = "$title";
+      	}
+          }
+
+          return implode($separator, $breadcrumbs);
+      }
+      echo breadcrumbs();
+
+      ?>
+    </div>
   </div>
 </nav>
